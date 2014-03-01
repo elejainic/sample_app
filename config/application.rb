9443f58d90fd565d14a0e8aca175f59e99f4960a
Jaime Minang <elejainic@gmail.com>
2014-03-01 11:05:08 -0500

    improve the README

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
+    config.assets.initialize_on_precompile = false
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
@@ -64,6 +65,6 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+
   end
 end
