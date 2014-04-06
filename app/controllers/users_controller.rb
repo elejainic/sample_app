90e8396a877e87cd155d56fb1ef24a5a58eab1c6
Jaime Minang <elejainic@gmail.com>
2014-04-06 09:11:24 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Bienvenido a Kedada, ahora a divertirte!"
+  		redirect_to user_path(@user)
+  	else
+  		render 'new'
+  	end
+  end
+
+
 
   
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,4 +11,50 @@ describe "User pages" do
 		it { should have_selector('title', content: full_title('Apuntate')) }
     
     end
+
+    describe "profile page" do
+    	let(:user) { FactoryGirl.create(:user) }
+	
+		before { visit user_path(user) }
+
+		it { should have_selector('h1', content: user.name) }
+		it { should have_selector('title', content: user.name) }
+	end
+
+	describe "signup" do
+		before { visit signup_path }
+
+		let(:submit) { "Crear mi cuenta" }
+		describe "with invalid information" do
+
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "Name", with: "Example User"
+				fill_in "Email", with: "user@example.com"
+				fill_in "Password", with: "foobar"
+				fill_in "Confirmation", with: "foobar"
+			end
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving a user" do
+
+				before { click_button submit}
+				
+				let(:user) { User.find_by_email("user@example.com")}
+
+				it { should have_selector('title', content: user.name) }
+			end
+
+
+
+		end
+	end
 end
+	
